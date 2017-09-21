14528349343916bb36164498a69ed3dc9263d030
Corky Brown <cbrown@themeetgroup.com>
2017-09-21 17:23:19 -0400

    Only deploy commands in automated script

--- automated_deploy.sh
+++ automated_deploy.sh
@@ -1,7 +1,6 @@
-node lib/google-search-by-city/index.js world
-
 git config --global user.email "galaxyblur@gmail.com"
 git config --global user.name "Corky Brown"
+git config --global push.default simple
 
 git add .
 git commit -m "Auto-commit data update"
