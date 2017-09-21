5f8c548eaf192ccd5b3fee8e030db4df06dbb666
Corky Brown <cbrown@themeetgroup.com>
2017-09-21 17:19:23 -0400

    Try build from sh file

---
+++ automated_build_world.sh
@@ -0,0 +1,8 @@
+node lib/google-search-by-city/index.js world
+
+git config --global user.email "galaxyblur@gmail.com"
+git config --global user.name "Corky Brown"
+
+git add .
+git commit -m "Auto-commit data update"
+git push
